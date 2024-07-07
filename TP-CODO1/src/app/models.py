206c2511a70c14ea3962f38fb2de72923b445fff
NicoBianchi97 <153780201+NicoBianchi97@users.noreply.github.com>
2024-07-07 20:18:39 -0300

    Delete TP-CODO1 directory

--- TP-CODO1/src/app/models.py
+++
@@ -1,8 +0,0 @@
-class Libro:
-    def __init__(self, id, nombre_libro, autor, precio, genero, imagen ):
-        self.id = id
-        self.nombre_libro = nombre_libro
-        self.autor = autor
-        self.precio = precio
-        self.genero = genero
-        self.imagen = imagen
